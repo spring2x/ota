b0b012e0d52b919f69b367559e0d1de371db2ede
tlxlggg <943625282@qq.com>
2017-09-19 17:00:20 +0800

    初始化dashboard

---
+++ ota_hystrix_dashboard/src/main/java/com/ota_hystrix_dashboard/Application.java
@@ -0,0 +1,15 @@
+package com.ota_hystrix_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class Application {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
